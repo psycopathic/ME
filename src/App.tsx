e9b414fc14fd81bf775cb9260b9f3c68736bb638
psycopathic <harshsrivastava2502@gmail.com>
2025-12-24 06:09:01 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import smokeVideo from './assets/smoke.mp4'
+
+function App() {
+
+  return (
+    <>
+      <section>
+        <video src={smokeVideo} autoPlay muted ></video>
+        <h1>
+          <span>P</span>
+          <span>S</span>
+          <span>Y</span>
+          <span>C</span>
+          <span>O</span>
+          <span>P</span>
+          <span>A</span>
+          <span>T</span>
+          <span>H</span>
+          <span>I</span>
+          <span>C</span>
+        </h1>
+      </section>
+    </>
+  )
+}
+
+export default App
