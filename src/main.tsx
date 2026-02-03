edf61068b6cca2616992fbeb1c48ff5c744d4a15
psycopathic <harshsrivastava2502@gmail.com>
2026-02-04 02:18:16 +0530

    remove the cursor

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
-import CursorTrail from "./components/CursorTrail.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-    <CursorTrail />
   </StrictMode>,
 );
